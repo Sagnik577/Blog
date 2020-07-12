4b3730a216d34f9edb7a85076f21552595bd48c5
Sagnik577 <sagnikdatta.agt@gmail.com>
2020-07-12 14:48:41 +0530

    URL changed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let posts=[];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://admin-ayan:radhaswami@ayans.pb3xa.mongodb.net/ComposeDB", {
+mongoose.connect(process.env.URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
